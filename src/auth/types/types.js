b2dcffd2e4c8b648b1e91f98c966483351a9a26c
Andres Correa <andrescolo2008gmail.com>
2023-08-15 15:11:28 -0500

    context y reducer de la APP

---
+++ src/auth/types/types.js
@@ -0,0 +1,6 @@
+// tener centralizados los diferentes tipos 
+
+export const types =  {
+ login:'[Auth] login',
+ logout:'[Auth] logout',
+}
--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -13,7 +13,8 @@ export const SearchPage = () => {
   const {q=''} = queryString.parse(location.search);
   const heroes=getHeroesByName(q)
   
-  
+  const showSearch=(q.length ===0)
+  const showError=(q.length > 0 ) && heroes.length === 0;
   
   const{searchText,onInputChange}= useForm({
     searchText:q
@@ -21,7 +22,7 @@ export const SearchPage = () => {
 
 const onSearchSubmit= (event)=>{
   event.preventDefault();
-if (searchText.trim().length <= 1) return
+// if (searchText.trim().length <= 1) return
   
   navigate(`?q=${searchText}`)
 }
@@ -56,12 +57,19 @@ if (searchText.trim().length <= 1) return
     <div className='col-7'>
       <h4>Results</h4>
       <hr />
+        {/* {
+          (q==='')
+           ? <div className='alert alert-priamry'> Search a hero </div>
+           :(heroes.length ===0)
+           && <div className='alert alert-danger'> No hero with <b>{q}</b>
+          </div>
+           } */}
 
-           <div className='alert alert-priamry'>
-            Search a hero 
+           <div className='alert alert-priamry animate__animated animate__fadeIn ' style={ {display:showSearch ? '' : 'none'} }>
+            Search a Hero
            </div>
 
-           <div className='alert alert-danger'>
+           <div className='alert alert-danger animate__animated animate__fadeIn ' style={ {display:showError ? '' : 'none'} }>
             No hero with <b>{q}</b>
            </div>
 
