93e78175b4473fbbe6ec566c140b26f008d92966
Andres Correa <andrescolo2008gmail.com>
2023-08-17 14:33:06 -0500

    17 proteger rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import {  Route, Routes } from "react-router-dom"
 import {  HeroesRoutes } from "../heroes"
 import {  LoginPage } from "../auth"
+import { PrivateRoute } from "./PrivateRoute"
 
 
 export const AppRouter = () => {
@@ -10,9 +11,17 @@ export const AppRouter = () => {
 <Routes>
   
   <Route path="login" element ={<LoginPage/>} />
-  <Route path="/*" element ={<HeroesRoutes/>} />
-  
 
+  <Route path="/*" element ={
+      <PrivateRoute>
+  
+             <HeroesRoutes/>
+  
+        </PrivateRoute>
+  } />
+
+  {/* <Route path="/*" element ={<HeroesRoutes/>} /> */}
+  
 </Routes>
 
     </>
