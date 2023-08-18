79908df38781e19b8c65be97f26eed8761add6d7
Andres Correa <andrescolo2008gmail.com>
2023-08-17 21:52:10 -0500

    20. imágenes cargadas correctamente

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -27,7 +27,7 @@ return (
       <div className="col-4">
 
             <img 
-            src={`/assets/heroes/${id}.jpg`} 
+            src={`/heroes/${id}.jpg`} 
             alt={hero.superhero}
             className="img-thumbnail animate__animated animate__fadeInLeft animate__slow	2s" 
             />
